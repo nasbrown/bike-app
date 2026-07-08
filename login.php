309fee1b9785e78fcfbc615ac16ad721eec3b8b6
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2026-07-08 19:59:47 -0400

    Login: Can open up the google page, now need to capture the redirect url

--- login.php
+++ login.php
@@ -1,10 +1,33 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+require 'includes/init.php';
+
+$client = new Google\Client;
+
+$client->setClientId(BIKE_CLIENT_ID);
+
+$client->setClientSecret(BIKE_CLIENT_SECRET);
+
+$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] .'/bike-app/redirect.php');
+
+$client->addScope("email");
+
+$client->addScope("profile");
+
+$url = $client->createAuthUrl();
+
+?>
+
+
 <?php require('includes/header.php') ?>
-    <section>
+<section>
+    <div>
+        <h1>Welcome! Sign In to use the Bike Parking App</h1>
         <div>
-            <h1>Welcome! Sign In to use the Bike Parking App</h1>
-            <div> 
-                <button>Sign In To Google <img src="/bike-app/images_icons/google-symbol.svg" alt="" srcset=""></button>
-            </div>
+            <a href="<?= $url ?>">Sign In To Google <img src="/bike-app/images_icons/google-symbol.svg" alt="" srcset=""></a>
         </div>
-    </section>
+    </div>
+</section>
 <?php require('includes/footer.php') ?>
