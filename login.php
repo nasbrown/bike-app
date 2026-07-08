12d547fdf4c926d8605c4397a93a7d615272f2a7
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2026-07-08 18:57:05 -0400

    Created login page and downloaded google api client package to insert google authentication into app

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php require('includes/header.php') ?>
+    <section>
+        <div>
+            <h1>Welcome! Sign In to use the Bike Parking App</h1>
+            <div> 
+                <button>Sign In To Google <img src="/bike-app/images_icons/google-symbol.svg" alt="" srcset=""></button>
+            </div>
+        </div>
+    </section>
+<?php require('includes/footer.php') ?>
