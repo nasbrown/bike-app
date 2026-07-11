c0afacb9ff613c646eac404d477b3bccaf3acafb
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2026-07-10 23:34:30 -0400

    Styling sign-in button, need to make the entire link clickable

--- index.php
+++ index.php
@@ -15,12 +15,14 @@ $url = $client->createAuthUrl();
 
 <?php require('includes/header.php') ?>
 <section class="sign-in">
-    <div>
+    <div class="sign-in-container">
         <h1>Welcome! Sign In to use the Bike Parking App</h1>
-        <div>
-            <a href= "<?= $url ?>">Sign In To Google <img src="/bike-app/images_icons/google-symbol.svg" alt="" srcset=""></a>
+        <div class="link-container">
+            <a href="<?= $url ?>">Sign In To Google</a>
+            <div class="img-container">
+                <img src="/bike-app/images_icons/google-symbol.svg" alt="" srcset="">
+            </div>
         </div>
     </div>
 </section>
 <?php require('includes/footer.php') ?>
-
